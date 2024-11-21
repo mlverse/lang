4f1fbac06b2a6ca37b329f63fa2f1d347dbd9145
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-21 09:49:34 -0600

    Creates R file

--- R/translate-roxygen.R
+++ R/translate-roxygen.R
@@ -1,15 +1,24 @@
 roxygen_translate <- function(path, lang = NULL, dir = fs::path("man-lang", lang)) {
-  path <- "R/lang-help.R"
+  path <- "~/Projects/mall/r/R/llm-classify.R"
   lang <- "fr"
   dir <- fs::path("man-lang", lang)
-  
-  if(is_file(path)) {
-    parsed <- roxygen2::parse_file(path)  
-    for(roxy in parsed) {
-      for(tag in roxy$tags) {
+  dir_create(dir)
+  if (is_file(path)) {
+    rd_path <- path(dir, path_file(path))
+    parsed <- roxygen2::parse_file(path)
+    contents <- NULL
+    for (roxy in parsed) {
+      for (tag in roxy$tags) {
         translation <- llm_vec_translate(tag$raw, language = lang)
-        print(glue("#' @{tag$tag} {translation}"))
+        # translation <- tag$raw
+        if (tag$tag != "title") {
+          contents <- c(contents, glue("#' @{tag$tag} {translation}"))
+        } else {
+          contents <- c(contents, glue("#' {translation}"))
+        }
       }
+      contents <- c(contents, glue("{roxy$object$alias} <- function(...) NULL"))
     }
+    writeLines(contents, rd_path)
   }
 }
