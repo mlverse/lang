0adeb4d839ed06d0e61679afc3c6d5de6e6065da
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-21 11:06:48 -0600

    Splits single file translation from batch

--- R/translate-roxygen.R
+++ R/translate-roxygen.R
@@ -1,27 +1,37 @@
-roxygen_translate <- function(path, lang = NULL, dir = fs::path("man-lang", lang)) {
-  path <- "~/Projects/mall/r/R/llm-custom.R"
-  lang <- "fr"
+roxygen_translate <- function(lang = NULL, path = "R", dir = fs::path("man-lang", lang)) {
   dir <- fs::path("man-lang", lang)
   dir_create(dir)
-  if (is_file(path)) {
-    rd_path <- path(dir, path_file(path))
-    parsed <- roxygen2::parse_file(path)
-    contents <- NULL
-    for (roxy in parsed) {
-      for (tag in roxy$tags) {
-        raw <- tag$raw
-        if(tag$tag %in% c("title", "description", "param", "details", "returns")) {
-          raw <- llm_vec_translate(raw, language = lang)
-        }
-        raw <- gsub("\n", "\n#'", raw)
-        if (tag$tag == "title") {
-          contents <- c(contents, glue("#' {raw}"))
-        } else {
-          contents <- c(contents, glue("#' @{tag$tag} {raw}"))
-        }
-      }
-      contents <- c(contents, glue("{roxy$object$alias} <- function(...) NULL"))
+  if (is_dir(path)) {
+    r_files <- dir_ls(path, glob = "*.R")
+    for (files in r_files) {
+      roxygen_translate_file(path = files, lang = lang, dir = dir)
     }
+  } else {
+    roxygen_translate_file(path = path, lang = lang, dir = dir)
+  }
+}
+
+roxygen_translate_file <- function(path, lang = NULL, dir = fs::path("man-lang", lang)) {
+  cli_inform("Translating: {path}")
+  rd_path <- path(dir, path_file(path))
+  parsed <- roxygen2::parse_file(path)
+  contents <- NULL
+  for (roxy in parsed) {
+    for (tag in roxy$tags) {
+      raw <- tag$raw
+      if (tag$tag %in% c("title", "description", "param", "details", "returns")) {
+        raw <- llm_vec_translate(raw, language = lang)
+      }
+      raw <- gsub("\n", "\n#'", raw)
+      if (tag$tag == "title") {
+        contents <- c(contents, glue("#' {raw}"))
+      } else {
+        contents <- c(contents, glue("#' @{tag$tag} {raw}"))
+      }
+    }
+    contents <- c(contents, glue("{roxy$object$alias} <- function(...) NULL"))
+  }
+  if (!is.null(contents)) {
     writeLines(contents, rd_path)
   }
 }
