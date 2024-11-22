a70295e0328f7522145ed92afb3d735b773c0903
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-22 13:10:56 -0600

    Switches code out to use 'folder' instead of 'lang'

--- R/process-roxygen.R
+++ R/process-roxygen.R
@@ -1,5 +1,5 @@
 #' Creates the Rd files based on translated Roxygen scripts
-#' @param folder Source sub-folder where the source Roxygen R scripts are 
+#' @param folder Source sub-folder where the source Roxygen R scripts are
 #' @param source Base source folder where the different translations are located.
 #' Defaults to 'man-lang'.
 #' @param target Base target folder where the different translations will be
@@ -21,19 +21,19 @@ process_roxygen_folder <- function(folder, source = "man-lang", target = "inst/m
   # Copies content of the translated script to the R folder
   # of the temp copy
   file_copy(
-    path = dir_ls(path(copy_path, source, lang)),
+    path = dir_ls(path(copy_path, source, folder)),
     new_path = path(copy_path, "R"),
     overwrite = TRUE
   )
   # Using callr to avoid the messages from roxygen2
-  cli_h3("Creating Rd files for '{lang}'")
+  cli_h3("Creating Rd files for '{folder}'")
   callr::r(
     func = function(x) roxygen2::roxygenize(x, roclets = "rd"),
     args = list(copy_path)
   )
   # Copies the new contents in 'man' from the temp copy
   # into target folder, under the language's sub-folder
-  target_path <- path(target, lang)
+  target_path <- path(target, folder)
   if (dir_exists(target_path)) {
     dir_delete(target_path)
   }
