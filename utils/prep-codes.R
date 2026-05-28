58ea5e36363317d2f131428c38c486c184c020d0
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2026-05-28 10:38:27 -0500

    Air updates

--- utils/prep-codes.R
+++ utils/prep-codes.R
@@ -2,29 +2,29 @@ library(tidyverse)
 
 iso <- read_csv("utils/iso_639-1.csv")
 
-codes <- iso |> 
+codes <- iso |>
   mutate(
     name = str_to_lower(name),
     code = `639-1`
-  ) |> 
+  ) |>
   select(name, code)
 
-clean_codes <- codes |> 
-  mutate(name = str_replace(name, "\\(farsi\\)", ", farsi")) |> 
-  mutate(name = str_remove(name, "\\(tonga islands\\)")) |> 
-  mutate(name = str_remove(name, "\\(saṁskṛta\\)")) |> 
-  mutate(name = str_remove(name, "\\(eastern\\)")) |> 
-  mutate(name = str_remove(name, "\\(marāṭhī\\)")) |> 
-  separate_longer_delim(name, ",") |> 
-  mutate(name = str_remove(name, "\\(modern\\)")) |> 
-  mutate(name = str_remove(name, "standard")) |> 
-  mutate(name = str_trim(name)) |> 
-  arrange(code) |> 
-  group_by(name, code) |> 
-  summarise() |> 
-  group_by(code) |> 
-  mutate(number = row_number()) |> 
-  ungroup() |> 
+clean_codes <- codes |>
+  mutate(name = str_replace(name, "\\(farsi\\)", ", farsi")) |>
+  mutate(name = str_remove(name, "\\(tonga islands\\)")) |>
+  mutate(name = str_remove(name, "\\(saṁskṛta\\)")) |>
+  mutate(name = str_remove(name, "\\(eastern\\)")) |>
+  mutate(name = str_remove(name, "\\(marāṭhī\\)")) |>
+  separate_longer_delim(name, ",") |>
+  mutate(name = str_remove(name, "\\(modern\\)")) |>
+  mutate(name = str_remove(name, "standard")) |>
+  mutate(name = str_trim(name)) |>
+  arrange(code) |>
+  group_by(name, code) |>
+  summarise() |>
+  group_by(code) |>
+  mutate(number = row_number()) |>
+  ungroup() |>
   arrange(code)
 
 write_rds(clean_codes, "inst/iso/codes.rds")
